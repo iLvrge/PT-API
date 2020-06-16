9ecb2eb239135d7bc27506c8ff99f453d8c1f86d
Uzi Aloush <uzi@synpat.com>
2020-06-16 13:09:02 -0700

    Login and Profile

--- routes/business/profile.js
+++ routes/business/profile.js
@@ -17,7 +17,7 @@ route.get("/profile", [authJWT.verifyToken], (req, res, next) => {
 
     User.findOne({
         where: {id: req.userId, status:0},
-        attributes: ['id','first_name', 'last_name', 'email_address', 'logo', 'job_title'],
+        attributes: [['user_id','id'],'first_name', 'last_name', 'email_address', 'logo', 'job_title'],
         include: [
             {
                 model: Role,
