ee9d9d039eee7ea85d5025f4ae60fd754459422c
Uzi Aloush <uzi@synpat.com>
2020-06-22 15:00:14 -0700

    Update find parties first check to find representative

--- helpers/helper.js
+++ helpers/helper.js
@@ -339,8 +339,11 @@ let findCompanyCustomersByName = async(companyName) => {
 
             if(listIDs != null && listIDs.length > 0) {
                 let assgnorAssigneeIDS = [];
-                listIDs.map(async c => await assgnorAssigneeIDS.push(c.assignor_and_assignee_id));
 
+                for(let i = 0; i< listIDs.length; i++){
+                    assgnorAssigneeIDS.push(listIDs[i].assignor_and_assignee_id);
+                }
+                console.log(assgnorAssigneeIDS);
                 /** Find Assignors */
 
                 let queryAssignor = "SELECT a.or_name as name, count(a.or_name) as counter, r.representative_name as normalize_name FROM db_uspto.assignor as a LEFT JOIN assignor_and_assignee as aaa ON aaa.assignor_and_assignee_id = a.assignor_and_assignee_id LEFT JOIN representative as r ON r.representative_id = aaa.representative_id INNER JOIN (SELECT rf_id FROM db_uspto.assignee as ac WHERE ac.assignor_and_assignee_id IN (:IDs)) as b ON  b.rf_id = a.rf_id GROUP BY a.or_name";
@@ -359,7 +362,7 @@ let findCompanyCustomersByName = async(companyName) => {
                 /** Find Assignees */
 
                 /*let queryAssignee = "SELECT ee.ee_name as name, count(ee.ee_name) as counter, r.representative_name as normalize_name from assignee as ee INNER JOIN (SELECT rf_id FROM db_uspto.assignor as ac INNER JOIN ( SELECT aa.assignor_and_assignee_id FROM assignor_and_assignee as aa LEFT JOIN representative as r1 ON r1.representative_id = aa.representative_id WHERE (r1.representative_name=:name OR aa.name = :name)) as np ON np.assignor_and_assignee_id = ac.assignor_and_assignee_id GROUP BY ac.rf_id) as temp ON temp.rf_id = ee.rf_id LEFT JOIN assignor_and_assignee as aaa ON aaa.assignor_and_assignee_id = ee.assignor_and_assignee_id LEFT JOIN representative as r ON r.representative_id = aaa.representative_id GROUP BY ee.ee_name";*/
-
+                console.log(assgnorAssigneeIDS);
                 let queryAssignee = "SELECT a.ee_name as name, count(a.ee_name) as counter, r.representative_name as normalize_name FROM db_uspto.assignee as a LEFT JOIN assignor_and_assignee as aaa ON aaa.assignor_and_assignee_id = a.assignor_and_assignee_id LEFT JOIN representative as r ON r.representative_id = aaa.representative_id INNER JOIN (SELECT rf_id FROM db_uspto.assignee as ac WHERE ac.assignor_and_assignee_id IN (:IDs)) as b ON  b.rf_id = a.rf_id GROUP BY a.ee_name";
 
 
