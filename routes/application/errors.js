dc6a382d8eb565c72c858198db55370ded0c0a25
Uzi Aloush <uzi@synpat.com>
2020-07-08 15:43:11 -0700

    Companies list sort according to transactions

--- routes/application/errors.js
+++ routes/application/errors.js
@@ -25,9 +25,9 @@ route.get("/errors/:type/:companyName", [authJWT.verifyToken, clientDBConnection
         }
     }
 
-    if(type == 'count') {     
-        if(companyName != 'undefined') {
-            if(typeof req.connection_db != "undefined" && req.connection_db != null ) {   
+    if(type == 'count') {  
+        if(typeof req.connection_db != "undefined" && req.connection_db != null ) {     
+            if(companyName != 'undefined' && companyName != 0) {
                 const queryFindParent = "SELECT representative_id FROM representative WHERE (original_name = :name OR representative_name = :name) AND parent_id = 0";
             
                 const findParent = await req.connection_db.query(queryFindParent,{
@@ -49,14 +49,19 @@ route.get("/errors/:type/:companyName", [authJWT.verifyToken, clientDBConnection
                     res.status(200).json({uspto: 0, patent: 0});
                 }
             } else {
-                res.status(200).json({uspto: 0, patent: 0});
+                const errorCount = await Errors.count({
+                    where: {organisation_id: req.orgId},
+                    col: 'error_id',                            
+                });
+                res.status(200).json({uspto: errorCount, patent: 0});
             }
         } else {
             res.status(200).json({uspto: 0, patent: 0});
         }
-    } else if(type == 'list') {
-        if(companyName != 'undefined') {
-            if(typeof req.connection_db != "undefined" && req.connection_db != null ) {   
+    } else if(type == 'list') {        
+        if(typeof req.connection_db != "undefined" && req.connection_db != null ) {
+
+            if(companyName != 'undefined' && companyName != 0) {
 
                 const queryFindParent = "SELECT representative_id FROM representative WHERE (original_name = :name OR representative_name = :name) AND parent_id = 0";
                 
@@ -98,7 +103,29 @@ route.get("/errors/:type/:companyName", [authJWT.verifyToken, clientDBConnection
                     res.status(200).json({invent:[], assign: [], corr: [], address: [], security: []});
                 }
             } else {
-                res.status(200).json({invent:[], assign: [], corr: [], address: [], security: []});
+                const queryError = "SELECT appno_doc_num FROM error as e WHERE e.organisation_id = :organisationID AND e.representative_id = :representative_id GROUP BY e.appno_doc_num";
+                const getErrors= await connection.application.query(queryError,{
+                    type: connection.Sequelize.QueryTypes.SELECT,
+                    raw: true,
+                    replacements: { organisationID: req.orgId},
+                    logging: console.log,
+                });  
+
+                if(getErrors != null && getErrors.length > 0) {
+                    const getList = [];
+                    getErrors.map(e => getList.push(e.appno_doc_num));
+
+                    const queryErrorList = "SELECT d.appno_doc_num as asset, date_format(ass.record_dt,'%m/%d/%Y') as created_at, ass.cname as name FROM documentid as d LEFT JOIN assignment as ass ON ass.rf_id = d.rf_id WHERE d.appno_doc_num IN(:appNo) GROUP BY d.appno_doc_num ORDER BY ass.record_dt DESC";
+
+                    getErrorList = await connection.application.query(queryErrorList,{
+                        type: connection.Sequelize.QueryTypes.SELECT,
+                        raw: true,
+                        replacements: { appNo: getList },
+                        logging: console.log,
+                        }
+                    );
+                }       
+                res.status(200).json({invent:getErrorList, assign: [], corr: [], address: [], security: []});    
             }
         } else {
             res.status(200).json({invent:[], assign: [], corr: [], address: [], security: []});
