74474bd7062e2f8e9d26a20e7bde0d49b6773c39
Uzi Aloush <uzi@synpat.com>
2020-07-16 20:54:54 -0700

    Tree parties

--- helpers/helper.js
+++ helpers/helper.js
@@ -457,7 +457,7 @@ let findCompanyCustomersByName = async(companyName, type) => {
             if(typeof type != 'undefined' &&  parseInt(type) == 2) {
                 let queryAssignee = "SELECT a.ee_name as name, count(a.ee_name) as counter, r.representative_name as normalize_name, (select rr.representative_name FROM representative as rr WHERE rr.representative_name = aaa.name GROUP BY rr.representative_name) as representativeCompany FROM db_uspto.assignee as a INNER JOIN assignment_conveyance as ac ON ac.rf_id = a.rf_id LEFT JOIN assignor_and_assignee as aaa ON aaa.assignor_and_assignee_id = a.assignor_and_assignee_id LEFT JOIN representative as r ON r.representative_id = aaa.representative_id WHERE a.rf_id IN (:IDs) ";
 
-                queryAssignee +=" AND ac.employer_assign in (0,1)"; 
+                //queryAssignee +=" AND ac.employer_assign in (0,1)"; 
             
 
             queryAssignee += " GROUP BY a.ee_name";
