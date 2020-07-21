a495ede1773ee1e457b4cd82cbe3624e33a7fed4
Uzi Aloush <uzi@synpat.com>
2020-07-20 18:24:16 -0700

    USPTO representative_assignment_conveyance

--- helpers/helper.js
+++ helpers/helper.js
@@ -105,7 +105,7 @@ let searchCompany = async(search) => {
             let companyData = {...company};
             companyData.children = [];
             searchResult.push(companyData);
-            allNames.push(company.normalize_name != null ? company.normalize_name : company.name)
+            allNames.push(company.name)
         })
 
         queryChildCompany = `SELECT a.assignor_and_assignee_id as id, a.name, sum(a.instances) as counter, c.representative_name as normalize_name, (select rr.representative_name FROM representative as rr WHERE rr.representative_name = a.name GROUP BY rr.representative_name) as representative_company FROM assignor_and_assignee as a INNER JOIN representative as c ON c.representative_id = a.representative_id where c.representative_name IN (:name)`;
@@ -120,9 +120,8 @@ let searchCompany = async(search) => {
 
         if(getChildCompanyData.length > 0) {
             getChildCompanyData.map(c => {
-                for(let i = 0; i < searchResult.length; i++) {
-                    let parentName = searchResult[i].normalize_name != null ? searchResult[i].normalize_name : searchResult[i].name;
-                    if(parentName == c.normalize_name){
+                for(let i = 0; i < searchResult.length; i++) {                    
+                    if(searchResult[i].name == c.normalize_name){
                         searchResult[i].children.push(c);
                         return;
                     }
