f4e473dcf25792825834b32ea0287abd9ee9a499
Uzi Aloush <uzi@synpat.com>
2020-07-27 12:11:46 -0700

    Categories for Assignments

--- helpers/helper.js
+++ helpers/helper.js
@@ -197,8 +197,11 @@ let findRepresentative = async (OrganisationName) => {
 let allAssignments = async (customerID) => {
     let queryAllAssignments = "", assignmentsList = [];
     if(parseInt(customerID) > 0) {
-        let org = findOrganisationbyID( customerID );
+        
+        let org = await findOrganisationbyID( customerID );
+        
         if(org != null && org.organisation_id > 0) {
+            console.log("ORG "+ org.organisation_id);
             const queryFindMainCompany = "SELECT aa.assignor_and_assignee_id, aa.name FROM assignor_and_assignee as aa LEFT JOIN representative as r1 ON r1.representative_id = aa.representative_id where (aa.name = :name OR r1.representative_name = :name )";
             const listIDs = await connection.application.query(queryFindMainCompany,{
                 type: connection.Sequelize.QueryTypes.SELECT,
@@ -208,11 +211,10 @@ let allAssignments = async (customerID) => {
                 }
             );
             if(listIDs != null && listIDs.length > 0) {
-                let assgnorAssigneeIDS = [], names = [];
+                let assgnorAssigneeIDS = [];
     
                 for(let i = 0; i< listIDs.length; i++){
                     assgnorAssigneeIDS.push(listIDs[i].assignor_and_assignee_id);
-                    names.push(listIDs[i].name);
                 }
                 console.log(assgnorAssigneeIDS);
                 /** Find Assignors */
@@ -243,7 +245,7 @@ let allAssignments = async (customerID) => {
                 rfIDsList.map( r => rfIDs.push(r.rf_id));
 
                 if(rfIDsList.length > 0) {
-                    queryAllAssignments = "Select a.rf_id as id, a.convey_text as text, CONCAT(ac.reel_no, '/', ac.frame_no) , ac.convey_ty, rac.convey_ty as updated_convey_ty from assignment as a INNER JOIN assignment_conveyance as ac ON ac.rf_id = a.rf_id LEFT JOIN representative_assignment_conveyance as rac ON rac.rf_id = a.rf_id WHERE a.convey_text <> '' AND a.convey_text IS NOT NULL AND a.rf_id IN (SELECT d.rf_id FROM documentid as d WHERE appno_doc_num <> '' AND  d.rf_id IN (:rfIDs) GROUP BY d.rf_id) ";
+                    queryAllAssignments = "Select a.rf_id as id, a.convey_text as text, CONCAT(a.reel_no, '/', a.frame_no) as reel_frame , ac.convey_ty, rac.convey_ty as updated_convey_ty from assignment as a INNER JOIN assignment_conveyance as ac ON ac.rf_id = a.rf_id LEFT JOIN representative_assignment_conveyance as rac ON rac.rf_id = a.rf_id WHERE a.convey_text <> '' AND a.convey_text IS NOT NULL AND a.rf_id IN (SELECT d.rf_id FROM documentid as d WHERE appno_doc_num <> '' AND  d.rf_id IN (:rfIDs) GROUP BY d.rf_id) ";
 
                     assignmentsList =  await connection.resources.query(queryAllAssignments,{
                         type: connection.Sequelize.QueryTypes.SELECT,
