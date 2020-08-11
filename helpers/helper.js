99bd16f57b4cb5936de532bca9acd80255e63f3f
Uzi Aloush <uzi@synpat.com>
2020-08-11 01:00:07 -0700

    Entities list

--- helpers/helper.js
+++ helpers/helper.js
@@ -521,7 +521,7 @@ let updateAllCustomerInventor = async(companyName, inventors, flag ) => {
     if(representativeName != '') {
         const rfIDs = [];
         if(flag == 0 ) {
-            const queryFindAssignorAndAssigneeIDs = "SELECT ac.rf_id FROM db_application.assignor as aaa INNER JOIN db_application.assignment_conveyance as ac ON ac.rf_id = aaa.rf_id WHERE aaa.rf_id IN(SELECT  a.rf_id FROM db_uspto.assignee as a WHERE a.assignor_and_assignee_id IN (SELECT aa.assignor_and_assignee_id FROM db_application.assignor_and_assignee as aa LEFT JOIN db_application.representative as r1 ON r1.representative_id = aa.representative_id where (r1.representative_name = :name OR aa.name = :name))) AND  aaa.or_name IN (:inventors) GROUP BY ac.rf_id";
+            const queryFindAssignorAndAssigneeIDs = "SELECT ac.rf_id FROM db_application.assignor as aaa INNER JOIN db_application.assignment_conveyance as ac ON ac.rf_id = aaa.rf_id WHERE aaa.assignor_and_assignee_id IN (SELECT aa.assignor_and_assignee_id FROM db_application.assignor_and_assignee as aa LEFT JOIN db_application.representative as r1 ON r1.representative_id = aa.representative_id where (r1.representative_name = :name OR aa.name = :name)) AND ac.employer_assign = 1 AND  aaa.or_name IN (:inventors) GROUP BY ac.rf_id";
             let listIDs = await connection.resources.query(queryFindAssignorAndAssigneeIDs,{
                 type: connection.Sequelize.QueryTypes.SELECT,
                 replacements: { name: representativeName, inventors: inventors },
@@ -706,9 +706,9 @@ let findCompanyCustomersByName = async(companyName, type) => {
 
                 if(typeof type != 'undefined' && parseInt(type) > 0) {
                     if(parseInt(type) == 1) {
-                        queryAssignor +=" AND (ac.employer_assign = 1 OR rac.employer_assign = 1) AND a.rf_id NOT IN (SELECT rf_id FROM db_uspto.representative_assignment_conveyance WHERE employer_assign = 0)";
+                        queryAssignor +=" AND (ac.employer_assign = 1 OR rac.employer_assign = 1) AND a.rf_id NOT IN (SELECT rf_id FROM db_uspto.representative_assignment_conveyance WHERE employer_assign = 0 AND rf_id IN (:IDs ))";
                     } else {
-                        queryAssignor +=" AND (ac.employer_assign = 0 OR rac.employer_assign = 0) AND a.rf_id NOT IN (SELECT rf_id FROM db_uspto.representative_assignment_conveyance WHERE employer_assign = 1)";
+                        queryAssignor +=" AND (ac.employer_assign = 0 OR rac.employer_assign = 0) AND a.rf_id NOT IN (SELECT rf_id FROM db_uspto.representative_assignment_conveyance WHERE employer_assign = 1 AND rf_id IN (:IDs ))";
                     }
                 }
 
