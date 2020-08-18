d5b60955c11dc859979d96585e936f4ec4c867b0
Uzi Aloush <uzi@synpat.com>
2020-08-18 12:43:24 -0700

    Updated Portfolios

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -184,7 +184,7 @@ route.post("/customers/:id/users", [authJWT.verifyToken, authJWT.isAdmin, userEx
                                 /** */
                                 (async () => {
                                     const clientDB = await clientDBConnection.connect(req, res, next);
-                                    consoel.log(clientDB);
+                                    console.log(clientDB);
                                     if(clientDB != null) {
                                         const clientUser = {
                                             user_id: user.user_id,
