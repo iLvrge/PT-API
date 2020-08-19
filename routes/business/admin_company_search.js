8502f33bfcffc5e36547228124b3d3f2d4c2e799
Uzi Aloush <uzi@synpat.com>
2020-08-19 16:33:48 -0700

    Update normalize company

--- routes/business/admin_company_search.js
+++ routes/business/admin_company_search.js
@@ -109,17 +109,18 @@ route.put("/company/search/all/", [authJWT.verifyToken, authJWT.isAdmin], (req, 
                     where:{name: normalize_name}
                 });
 
-                if(findNormalizedCompany != null && findNormalizedCompany.representative_name > 0) {
-                    const representativeCompany  = await Representatives.findOne({
+                console.log(findNormalizedCompany);
+
+                if(findNormalizedCompany != null && findNormalizedCompany.representative_id > 0) {
+                    representativeCompany  = await Representatives.findOne({
                         where:{representative_id: findNormalizedCompany.representative_id}
                     });
 
-                    if(findNormalizedCompanyData != null && findNormalizedCompanyData.representative_id > 0){
+                    if(representativeCompany != null && representativeCompany.representative_id > 0){
                         await Representatives.update({
                             representative_name: normalize_name
-                        }, {where: {representative_id: findNormalizedCompanyData.representative_id} });
+                        }, {where: {representative_id: representativeCompany.representative_id} });
                     }
-                    representativeCompany = await helpers.checkRepresentativeCompany(normalize_name);
                 }
 
                 
