fb961947bf7164462c6e6b9b4be583d0a93b36d7
Uzi Aloush <uzi@synpat.com>
2020-09-24 00:51:55 -0700

    Transaction for Assignor and Assignee

--- routes/business/admin_tree.js
+++ routes/business/admin_tree.js
@@ -54,7 +54,13 @@ route.post("/corporate_tree", [authJWT.verifyToken, authJWT.isAdmin], async(req,
                         return res.status(500).send("ERROR: "+err);	
                     } else {
 
-                        res.status(200).send(filePathWithName.replace('/var/www/html/beta','https://patentrack.com'));
+
+                        const contents = await fs.readFileSync(filePathWithName, 'utf8');
+
+                        res.status(200).send(contents);
+
+                        
+                        //res.status(200).send(filePathWithName.replace('/var/www/html/beta','https://patentrack.com'));
 
 
                         /*JSDOM.fromFile(filePathWithName).then(dom => {
