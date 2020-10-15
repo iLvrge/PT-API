0f94beb4321acffa9993a48566a144437bc3225a
Uzi Aloush <uzi@synpat.com>
2020-10-15 10:17:28 -0700

    Added CRUD for state keywords

--- routes/business/admin_company_search.js
+++ routes/business/admin_company_search.js
@@ -992,7 +992,8 @@ route.get("/company/raw/assignments/:id", [authJWT.verifyToken, authJWT.isAdmin,
                     {caddress_1: {[connection.Op.ne]: ''}},
                     {caddress_2: {[connection.Op.ne]: ''}}
                 ]
-            },  */      
+            },  */  
+            group:['cname', 'caddress_1', 'caddress_2','caddress_7','caddress_5','caddress_6','caddress_3','caddress_4'],    
             include: [
                 {
                     model: RepresentativeTransactions,
