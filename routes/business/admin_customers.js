6f639ea746971c216fdbd5842e4cab2b728d3ab0
Uzi Aloush <uzi@synpat.com>
2020-11-16 15:18:19 -0800

    Fix user listing with telephone

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -167,6 +167,7 @@ route.get("/customers/:id/users", [authJWT.verifyToken, authJWT.isAdmin, authJWT
                     /* const list = await helpers.getAllUsers(organisation.organisation_id);
                     res.status(200).json(list); */
                     if(typeof req.connection_db != "undefined" && req.connection_db != null ) {
+                        const dbUser = await req.connection_db.define('Users', ClientUsers.mainStructure, ClientUsers.options);
                         const list = await dbUser.findAll();
                         res.status(200).json(list);
                     }
