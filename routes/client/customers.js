66fb7a259415290e31952c1f41713473a8a7f1f2
Uzi Aloush <uzi@synpat.com>
2021-04-07 15:54:30 -0700

    Added broken title test

--- routes/client/customers.js
+++ routes/client/customers.js
@@ -609,6 +609,20 @@ route.get("/:type/assets", [authJWT.verifyToken, clientDBConnection.connect], as
                 }
                 res.status(200).json(assets);
             })
+        }  else if(req.params.type == 'market_review') {
+            connection.application.query("CALL `GetAssetsTableD`(:companies, :organisationID);",{
+                type: connection.Sequelize.QueryTypes.SELECT,
+                raw: true,
+                logging: console.log,
+                replacements: {companies: replacements.companies, organisationID: req.orgId},
+                }
+            ).spread(result => {
+                if (result) {
+                    assets.list = Object.values(result)
+                    assets.total_records = assets.list.length
+                }
+                res.status(200).json(assets);
+            })
         } else {
             connection.application.query("CALL `GetAssets`(:companies, :organisationID, :tabs, :customers, :assignments, :type);",{
                 type: connection.Sequelize.QueryTypes.SELECT,
