90eb9acdb7c6cdde209c0d811e55cb8fe989ca07
Uzi Aloush <uzi@synpat.com>
2021-04-19 11:51:21 -0700

    change env

--- routes/client/documents.js
+++ routes/client/documents.js
@@ -38,7 +38,9 @@ const oauth2Client = new google.auth.OAuth2(
 
 let authenticateGoogleToken = async( code ) => {
     let getTokens = {}
-
+    console.log(process.env.GOOGLE_CLIENT_ID,
+        process.env.GOOGLE_SECRET_KEY,
+        process.env.REDIRECT_URL)
     try{
         const {tokens} = await oauth2Client.getToken(code)
         getTokens = tokens
