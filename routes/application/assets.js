dba1c79e876a17b97a82c0b9a672fcf7ca9a4681
Uzi Aloush <uzi@synpat.com>
2021-05-10 13:19:30 -0700

    update asset document query

--- routes/application/assets.js
+++ routes/application/assets.js
@@ -77,7 +77,7 @@ route.get("/assets/:patentNumber/files/:channelID/slack/:token", [authJWT.verify
             } */
 
 
-            let query = 'SELECT assignment.rf_id as id, "usptodrive" as external_type, date_format(assignor.exec_dt, "%m-%d-%Y") as title, representative_assignment_conveyance.convey_ty, CASE WHEN assignment.status = 1 THEN CONCAT("https://s3-us-west-1.amazonaws.com/static.patentrack.com/assignments/var/www/html/beta/resources/shared/data/assignment-pat-",reel_no,"-",frame_no,".pdf") ELSE CONCAT("https://legacy-assignments.uspto.gov/assignments/assignment-pat-",reel_no,"-",frame_no,".pdf") END as url_private, (SELECT sum(no_of_parties) FROM report_representative_assets_transactions_parties WHERE report_representative_assets_transactions_parties.rf_id = assignment.rf_id GROUP BY report_representative_assets_transactions_parties.rf_id ) as count_parties, (SELECT assignee FROM report_representative_assets_transactions WHERE report_representative_assets_transactions.rf_id = assignment.rf_id LIMIT 1) as assignee, (SELECT assignor FROM report_representative_assets_transactions WHERE report_representative_assets_transactions.rf_id = assignment.rf_id LIMIT 1) as assignor FROM assignment INNER JOIN assignor ON assignor.rf_id = assignment.rf_id INNER JOIN documentid ON documentid.rf_id = assignment.rf_id INNER JOIN representative_assignment_conveyance ON representative_assignment_conveyance.rf_id = assignment.rf_id'
+            let query = 'SELECT assignment.rf_id as id, "usptodrive" as external_type, date_format(assignor.exec_dt, "%m-%d-%Y") as title, CASE WHEN representative_assignment_conveyance.convey_ty = "assignment" THEN "Ownership" WHEN representative_assignment_conveyance.convey_ty = "addresschg" THEN "Address Change" WHEN representative_assignment_conveyance.convey_ty = "namechg" THEN "Name Change" WHEN representative_assignment_conveyance.convey_ty = "partialassignment" THEN "Ownership" WHEN representative_assignment_conveyance.convey_ty = "release" THEN "Security Release"  ELSE representative_assignment_conveyance.convey_ty END as convey_ty, CASE WHEN assignment.status = 1 THEN CONCAT("https://s3-us-west-1.amazonaws.com/static.patentrack.com/assignments/var/www/html/beta/resources/shared/data/assignment-pat-",reel_no,"-",frame_no,".pdf") ELSE CONCAT("https://legacy-assignments.uspto.gov/assignments/assignment-pat-",reel_no,"-",frame_no,".pdf") END as url_private, (SELECT sum(no_of_parties) FROM report_representative_assets_transactions_parties WHERE report_representative_assets_transactions_parties.rf_id = assignment.rf_id GROUP BY report_representative_assets_transactions_parties.rf_id ) as count_parties, (SELECT assignee FROM report_representative_assets_transactions WHERE report_representative_assets_transactions.rf_id = assignment.rf_id LIMIT 1) as assignee, (SELECT assignor FROM report_representative_assets_transactions WHERE report_representative_assets_transactions.rf_id = assignment.rf_id LIMIT 1) as assignor FROM assignment INNER JOIN assignor ON assignor.rf_id = assignment.rf_id INNER JOIN documentid ON documentid.rf_id = assignment.rf_id INNER JOIN representative_assignment_conveyance ON representative_assignment_conveyance.rf_id = assignment.rf_id'
 
             if(type == 0) {
                 query += ' WHERE appno_doc_num =:appno_doc_num '                
@@ -223,7 +223,7 @@ route.post("/assets/move",[authJWT.verifyToken], async (req, res) => {
                             status: 0
                         })
                     }
-                    query.push(`(grant_doc_num = :grant${index} AND appno_doc_num = :appno${a} AND layout_id = :layout${a} AND status = :status${a}) `)
+                    query.push(`(grant_doc_num = :grant${a} AND appno_doc_num = :appno${a} AND layout_id = :layout${a} AND status = :status${a}) `)
                     queryCondition[`grant${a}`] = row.grant_doc_num
                     queryCondition[`appno${a}`] = row.appno_doc_num
                     queryCondition[`layout${a}`] = layout_id
@@ -235,9 +235,8 @@ route.post("/assets/move",[authJWT.verifyToken], async (req, res) => {
                         queryCondition[`appno${a}`] = row.appno_doc_num
                         queryCondition[`layout${a}`] = row.currentLayout
                         queryCondition[`status${a}`] = 0
+                        a++
                     }
-                    a++
-                    
                 })
                 addedBulkData = await AssetsTransfer.bulkCreate(assets)  
                 if(addedBulkData) {                    
