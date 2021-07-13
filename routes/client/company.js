dcf443d829233af7910d26be045e4f9162e69acb
Uzi Aloush <uzi@synpat.com>
2021-07-13 15:11:46 -0700

    BUtton status according to every account

--- routes/client/company.js
+++ routes/client/company.js
@@ -79,7 +79,7 @@ route.get("/summary", [authJWT.verifyToken, clientDBConnection.connect], async(r
 
     await Promise.all(promises)
 
-    const query = `SELECT ${allCompanies.length} as companies, sum(no_of_activities) as activites, sum(no_of_parties) as parties,  sum(no_of_inventor) as inventors, sum(no_of_transactions) as transactions, sum(no_of_assets) as assets, (SELECT sum(no_of_parties) - sum(no_of_transactions), 0 as documents FROM admin_representative_reports WHERE representative_name IN (:representativeName)) as arrows FROM representative_reports WHERE representative_name IN (:representativeName)`
+    const query = `SELECT ${allCompanies.length} as companies, sum(no_of_activities) as activites, sum(no_of_parties) as parties,  sum(no_of_inventor) as inventors, sum(no_of_transactions) as transactions, sum(no_of_assets) as assets, 0 as documents, (SELECT sum(no_of_parties) - sum(no_of_transactions) FROM admin_representative_reports WHERE representative_name IN (:representativeName)) as arrows FROM representative_reports WHERE representative_name IN (:representativeName)`
 
     report = await connection.resources.query(query,{
         type: connection.Sequelize.QueryTypes.SELECT,
