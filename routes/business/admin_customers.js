e3bbec05645fc0eee1bdf2b809c3ce477017b8b5
Uzi Aloush <uzi@synpat.com>
2021-08-12 03:58:29 -0700

    Added reports data

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -141,7 +141,7 @@ route.get("/customers/run_query/:representative_name/:query_no", [authJWT.verify
 route.get("/customers", [authJWT.verifyToken, authJWT.isAdmin], (req, res, next) => {
 
     Organisations.findAll({
-        attributes: [['organisation_id', 'id'], 'name','logo', [connection.Sequelize.literal(0, 'assets'), 'assets'], [connection.Sequelize.literal(0, 'no_of_transactions'),'no_of_transactions'], [connection.Sequelize.literal(0, 'no_of_parties'), 'no_of_parties'], [connection.Sequelize.literal(0, 'product'), 'product']],
+        attributes: [['organisation_id', 'id'], 'name','logo', 'organisation_type', [connection.Sequelize.literal(0, 'assets'), 'assets'], [connection.Sequelize.literal(0, 'no_of_transactions'),'no_of_transactions'], [connection.Sequelize.literal(0, 'no_of_parties'), 'no_of_parties'], [connection.Sequelize.literal(0, 'product'), 'product']],
         where: {type:{[connection.Op.ne]: 2}}
     })
     .then((list)=>{
