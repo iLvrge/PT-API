95489f9d6142109a3e04678554b470f9fcc87b6c
Vivek Kapoor <er.vivek2512@gmail.com>
2022-05-10 23:24:54 +0530

    Jurisdiction Family query

--- routes/client/customers.js
+++ routes/client/customers.js
@@ -618,7 +618,7 @@ route.post("/asset_types/assets/family", [authJWT.verifyToken, clientDBConnectio
 
         const list = await helpers.findFilterAssets(req);
         console.log('list', list)
-        let result = [['Country', 'Popularity']]
+        let result = [['Country', 'Assets']]
         if(list != '' && Array.isArray(list) && list.length > 0) {
             const query = `SELECT application_country, SUM(country_count) AS number FROM( SELECT  application_country, COUNT(application_country)  AS country_count FROM db_uspto.assets_family WHERE grant_doc_num IN (SELECT grant_doc_num FROM db_uspto.documentid WHERE appno_doc_num IN (:list) AND grant_doc_num <> '' GROUP BY grant_doc_num) AND application_country <> 'WO' GROUP BY application_number, application_country) AS temp GROUP BY application_country ORDER BY number DESC`;
 
