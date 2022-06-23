74aa236890f851769d67735990ffd7cf0cb1100d
Vivek Kapoor <er.vivek2512@gmail.com>
2022-06-24 00:48:18 +0530

    Fixed queries

--- routes/application/dashboards.js
+++ routes/application/dashboards.js
@@ -399,8 +399,12 @@ route.post("/", [authJWT.verifyToken], async(req, res, next) => {
                         AND dt.representative_id IN (:company_id)
                         GROUP BY assets.appno_doc_num) AS temp GROUP BY year) AS temp1`;
                     } else {
-                        query = `SELECT COUNT(application) AS number, application, patent, rf_id, total FROM (SELECT application, patent, rf_id, total FROM dashboard_items 
-                            WHERE type = :type AND organisation_id = :organisationID ${companies.length > 0 ? ' AND representative_id IN (:company_id) ' : ''} GROUP BY application) AS temp`
+                        let countQ = `COUNT(application) AS number,`, groupBy = `GROUP BY application`
+                        if(parseInt(qType) === 22) {
+                            countQ = `COUNT(patent) AS number,`, groupBy = `GROUP BY patent`
+                        }
+                        query = `SELECT ${countQ}, application, patent, rf_id, total FROM (SELECT application, patent, rf_id, total FROM dashboard_items 
+                            WHERE type = :type AND organisation_id = :organisationID ${companies.length > 0 ? ' AND representative_id IN (:company_id) ' : ''} ${groupBy}) AS temp`
                     }                    
                     break;                    
                 case 17:
