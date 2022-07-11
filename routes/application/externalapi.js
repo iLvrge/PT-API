0b71d3ac9e2f87e683f8a2900ec8292700405d34
Vivek Kapoor <er.vivek2512@gmail.com>
2022-07-11 21:54:08 +0530

    PDF Generator

--- routes/application/externalapi.js
+++ routes/application/externalapi.js
@@ -8,7 +8,11 @@ const express = require("express"),
 
     connection = require("../../config/db.config"),
 
-    OrganisationApplication = require("../../model/application/OrganisationApplication");
+    OrganisationApplication = require("../../model/application/OrganisationApplication"),
+
+    fileSystem = require('fs'),
+
+    generatePdfThumbnails = require('pdf-thumbnail');
     
 const { v4: uuidv4  } = require('uuid');
 
@@ -284,5 +288,31 @@ route.post("/citation", [authJWT.verifyToken], async (req, res) => {
         res.status(500).send('Error while rendering asset details')
     }
 })
+
+
+route.get('/generate_thumbnail', async (req, res) => {
+    const pdfURL = req.query.file
+    try {
+        generatePdfThumbnails(fileSystem.readFileSync('/var/www/html/trash/assignment-pat-49940-821.pdf', {
+            compress: {
+              type: 'PNG',  
+              quality: 90  
+            },
+            resize: {
+                width: 150,   //default
+                height: 150,  //default
+            }
+        })).then((data) /*is a stream*/ =>{
+            const stream = data.pipe(fileSystem.createWriteStream("/var/www/html/trash/assignment-pat-49940-821.png"))
+            stream.on('finish', function () { 
+                res.status(200).send(`https://php.patentrack.com/assignment-pat-49940-821.png`);
+            });
+        });
+        
+    } catch (err) {
+        console.error(err);
+    }
+})
+
 
 module.exports = route;
