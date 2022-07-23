a87bc7c60b057d1ccc7fd5ffd16f5bff04288bdf
Vivek Kapoor <er.vivek2512@gmail.com>
2022-07-23 09:28:25 +0530

    Added Release data in timeline and in tooltip

--- routes/application/dashboards.js
+++ routes/application/dashboards.js
@@ -532,12 +532,10 @@ route.post("/", [authJWT.verifyToken], async(req, res, next) => {
                 case 31:
                 case 32:
                 case 33:
+                    case 34:
                 case 36:
                     query = `SELECT COUNT(IF(patent <> '', patent, null)) AS number, COUNT(IF(patent = '', application, null)) AS other_number, COUNT(*) AS total, '' AS rf_id, type FROM dashboard_items WHERE type = :type AND organisation_id = :organisationID ${companies.length > 0 ? ' AND representative_id IN (:company_id) ' : ''}`
                     break;   
-                case 34:
-                    query = `SELECT COUNT(id) AS number, '' AS rf_id, 0 AS total, type FROM dashboard_items WHERE type = :type AND organisation_id = :organisationID ${companies.length > 0 ? ' AND representative_id IN (:company_id) ' : ''}`
-                    break;    
                 case 35:
                     query = `SELECT SUM(total) AS number, application, '' AS patent, '' AS rf_id, 0 AS total, type FROM dashboard_items WHERE type = :type AND organisation_id = :organisationID ${companies.length > 0 ? ' AND representative_id IN (:company_id) ' : ''}`
                     break;
