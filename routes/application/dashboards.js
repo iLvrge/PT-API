70e98d621701519cbbbc9eb4c5d8f8f87e093445
Vivek Kapoor <er.vivek2512@gmail.com>
2022-08-13 02:55:22 +0530

    Top Lenders

--- routes/application/dashboards.js
+++ routes/application/dashboards.js
@@ -528,7 +528,13 @@ route.post("/", [authJWT.verifyToken], async(req, res, next) => {
                 case 37: */
                 case 38:
                     if(ownedAssets.length > 0) {
-                        query = `SELECT cwc.name AS name, COUNT(application_country) AS number, grant_doc_num AS patent, '' AS application, '' AS rf_id, 0 AS total  FROM (
+                        query = `SELECT cwc.name AS name, COUNT(application_country) AS number, (SELECT grant_doc_num FROM (SELECT grant_doc_num, COUNT(grant_doc_num) as counter  FROM (
+                            SELECT grant_doc_num, application_number, application_country FROM db_uspto.assets_family AS af WHERE grant_doc_num IN (
+                                SELECT grant_doc_num FROM db_uspto.documentid AS di WHERE appno_doc_num IN (:list)                                     
+                                GROUP BY grant_doc_num
+                            ) AND application_country NOT IN ('WO', 'US') GROUP BY application_number) AS temp 
+                            INNER JOIN db_uspto.country_with_codes AS cwc ON cwc.country_code = temp.application_country 
+                            GROUP BY grant_doc_num ORDER BY counter DESC LIMIT 1) AS temp) AS patent, '' AS application, '' AS rf_id, 0 AS total  FROM (
                                 SELECT grant_doc_num, application_number, application_country FROM db_uspto.assets_family AS af WHERE grant_doc_num IN (
                                     SELECT grant_doc_num FROM db_uspto.documentid AS di WHERE appno_doc_num IN (:list)                                     
                                     GROUP BY grant_doc_num
