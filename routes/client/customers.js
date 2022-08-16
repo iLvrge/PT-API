abebf6f2df76ead7f3caa2c309b885432da2bd24
Vivek Kapoor <er.vivek2512@gmail.com>
2022-08-16 23:50:20 +0530

    Fixed UnAssigned Assets

--- routes/client/customers.js
+++ routes/client/customers.js
@@ -1171,7 +1171,7 @@ route.get("/:layout/transactions", [authJWT.verifyToken, clientDBConnection.conn
             companies = JSON.parse( companies )            
         }
         
-        if(replacements.layoutID == 17 || replacements.layoutID == 18 || replacements.layoutID == 19 ||replacements.layoutID == 24 || replacements.layoutID == 25 || replacements.layoutID == 26) {
+        if(replacements.layoutID == 17 || replacements.layoutID == 18 || replacements.layoutID == 19 ||replacements.layoutID == 24 || replacements.layoutID == 25 || replacements.layoutID == 26 || replacements.layoutID == 39 || replacements.layoutID == 40 || replacements.layoutID == 41) {
             if(companies.length > 0) {
                 replacements.companies = companies
             }
