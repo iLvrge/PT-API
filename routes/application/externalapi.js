a03d85cf3a4666f30f8cc31fb872973b85dfbee7
Vivek Kapoor <er.vivek2512@gmail.com>
2022-12-09 01:29:32 +0530

    Update whole account with selected companies

--- routes/application/externalapi.js
+++ routes/application/externalapi.js
@@ -381,7 +381,7 @@ route.post("/citation", [authJWT.verifyToken], async (req, res) => {
                 })
             }
             if( list.length > 0 ) {
-                let queryCitedLogo = "SELECT cp.cited_patent_id AS id, cpwa.citing_patent_number AS number, MAX(o.organisation_name) AS assignee, MAX(o.logo_optimize) AS logo, COUNT(cpwa.citing_patent_number) AS combined, AS combined, GROUP_CONCAT(o.organisation_name) AS all_assignee, cpwa.app_date AS start, cpwa.app_date AS end FROM cited_patents AS cp INNER JOIN assignee_organizations AS ao ON ao.assignee_id = cp.assignee_id INNER JOIN citing_patents_with_assignee AS cpwa ON cpwa.assignee_id = ao.assignee_id AND cpwa.patent_number = cp.patent_number LEFT JOIN organisations AS o ON o.organisation_id = ao.organisation_id WHERE cp.patent_number IN (:list) GROUP BY cp.patent_number, cpwa.citing_patent_number"
+                let queryCitedLogo = "SELECT cp.cited_patent_id AS id, cpwa.citing_patent_number AS number, MAX(o.organisation_name) AS assignee, MAX(o.logo_optimize) AS logo, COUNT(cpwa.citing_patent_number) AS combined, GROUP_CONCAT(o.organisation_name) AS all_assignee, cpwa.app_date AS start, cpwa.app_date AS end FROM cited_patents AS cp INNER JOIN assignee_organizations AS ao ON ao.assignee_id = cp.assignee_id INNER JOIN citing_patents_with_assignee AS cpwa ON cpwa.assignee_id = ao.assignee_id AND cpwa.patent_number = cp.patent_number LEFT JOIN organisations AS o ON o.organisation_id = ao.organisation_id WHERE cp.patent_number IN (:list) GROUP BY cp.patent_number, cpwa.citing_patent_number"
                 citedCompanies =  await connection.applicationNew.query(queryCitedLogo,{
                     type: connection.Sequelize.QueryTypes.SELECT,
                     raw: true,
