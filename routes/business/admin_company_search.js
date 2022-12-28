642ac74f393c468031e33f3bcdd34ef00b0e489c
Vivek Kapoor <er.vivek2512@gmail.com>
2022-12-28 12:12:59 +0530

    Citing Assignee Owned Assets

--- routes/business/admin_company_search.js
+++ routes/business/admin_company_search.js
@@ -1164,13 +1164,13 @@ route.get("/company/:companyID/law_firms", [authJWT.verifyToken, authJWT.isAdmin
             allIDs.push(req.params.companyID)
         }
         
-        const query = `SELECT law_firms.law_firm_id, law_firms.name,  COUNT(cor.rf_id) AS counter, law_firms.instances AS total_occurences, representative_law_firm.representative_id, representative_law_firm.representative_name 
+        const query = `SELECT law_firms.law_firm_id, law_firms.name AS name,  COUNT(cor.rf_id) AS counter, law_firms.instances AS total_occurences, representative_law_firm.representative_id, representative_law_firm.representative_name 
         FROM db_uspto.law_firm AS law_firms 
         LEFT JOIN db_uspto.representative_law_firm AS representative_law_firm ON representative_law_firm.representative_id =  law_firms.representative_id 
                 INNER JOIN correspondent AS cor ON cor.cname = law_firms.name 
                 INNER JOIN assignee ON assignee.rf_id = cor.rf_id 
                 WHERE assignee.assignor_and_assignee_id IN (:allIDs)
-        GROUP BY law_firms.law_firm_id`
+        GROUP BY law_firms.name`
 
         const findAllLawFirms = await connection.resources.query(query,{
             type: connection.Sequelize.QueryTypes.SELECT,
