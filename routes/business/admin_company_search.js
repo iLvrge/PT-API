34d5cba61b6010b73178301855b9ecdd4d824cec
Vivek Kapoor <er.vivek2512@gmail.com>
2023-04-03 10:33:02 +0530

    Adding bulk companies

--- routes/business/admin_company_search.js
+++ routes/business/admin_company_search.js
@@ -2955,9 +2955,9 @@ route.post("/company/:id/add_bulk_companies", [authJWT.verifyToken, authJWT.isAd
                             }
                         }
                         if(addRecord > 0) { 
-                            console.log(mainCompanies);
+                            console.log(parentCompaniesID, mainCompanies);
                             if(mainCompanies.length > 0){
-                                await exec(`php -f /var/www/html/trash/run_add_companies_script.php "${client_id}" "${JSON.stringify(parentCompaniesID)}"`, async (error, stdout, stderr) => {
+                                await exec(`php -f /var/www/html/trash/run_add_companies_script.php "${client_id}" '${JSON.stringify(parentCompaniesID)}'`, async (error, stdout, stderr) => {
                                     console.log(error);
                                     console.log(stdout);
                                     console.log(stderr);
@@ -3015,7 +3015,7 @@ route.post("/company/:id/add_bulk_companies", [authJWT.verifyToken, authJWT.isAd
                         if(addRecord > 0) {
                             console.log(mainCompanies);
                             if(mainCompanies.length > 0){
-                                await exec(`php -f /var/www/html/trash/run_add_companies_script.php "${client_id}" "${JSON.stringify(parentCompaniesID)}"`, async (error, stdout, stderr) => {
+                                await exec(`php -f /var/www/html/trash/run_add_companies_script.php "${client_id}" '${JSON.stringify(parentCompaniesID)}'`, async (error, stdout, stderr) => {
                                     console.log(error);
                                     console.log(stdout);
                                     console.log(stderr);
