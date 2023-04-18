0baea1669bf87ae114d399e43b02ac94a200f102
Vivek Kapoor <er.vivek2512@gmail.com>
2023-04-19 02:59:00 +0530

    Fixed correspondence all similar value update

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -2158,7 +2158,7 @@ route.post("/customers/retrieve_cited_patents_logo",[authJWT.verifyToken, authJW
     try {
 
         const {client_id, api_name, assignees, all, company_id, type, source_data} = req.body
-        console.log('retrieve_cited_patents_logo', '/var/www/html/script/name_to_domain_api.js', client_id, api_name, assignees, 1, company_id, all, type, source_data)
+        console.log('retrieve_cited_patents_logo')
         /* exec(`node /var/www/html/script/name_to_domain_api.js ${client_id} ${api_name} ${assignees} 1 > name_to_domain_api.log  2>&1`, function(err, stdout, stderr){
             console.log(`assigneeLogos downloadFileSpawn.stdout: ${stdout}`)
             console.log(`Error assigneeLogos downloadFileSpawn.stderr: ${stderr}`)
@@ -2170,7 +2170,7 @@ route.post("/customers/retrieve_cited_patents_logo",[authJWT.verifyToken, authJW
         /* const assigneeLogos = spawn('node', ['/var/www/html/script/name_to_domain_api.js', client_id, api_name, assignees, 1, company_id, all, type, source_data]); */
         spawn('node', ['/var/www/html/script/name_to_domain_api.js', client_id, api_name, assignees, 1, company_id, all, type, source_data]);
     
-        assigneeLogos.stdout.on('data', (data) => {
+        /* assigneeLogos.stdout.on('data', (data) => {
             logger.info(data)
             console.log(`assigneeLogos downloadFileSpawn.stdout: ${data}`)
         });
@@ -2183,7 +2183,7 @@ route.post("/customers/retrieve_cited_patents_logo",[authJWT.verifyToken, authJW
         assigneeLogos.on('close', (code) => {
             logger.info(code)
             resolve(`download assigneeLogos downloadFileSpawn.close ${code}`)    
-        })  
+        })  */
     
     
         res.status(200).send("run logo script");
