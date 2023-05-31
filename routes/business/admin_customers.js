e8a1bc995307479448b2b9dfb1324ec27a4bb43a
Vivek Kapoor <er.vivek2512@gmail.com>
2023-06-01 00:05:31 +0530

    Added ShareLinkDetails

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -654,7 +654,7 @@ route.get("/customers/:id/companies", [authJWT.verifyToken, authJWT.isAdmin, aut
 route.delete("/customers/:id/companies", [authJWT.verifyToken, authJWT.isAdmin, authJWT.addClientID, clientDBConnection.connect], async(req, res, next) => {
     try{
         let IDs = req.query.companies;
-        if(IDs.length == 0) {
+        if(IDs === undefined || IDs.length == 0) {
             IDs = req.payload.companies;
         }
         if(IDs.length > 0) {
