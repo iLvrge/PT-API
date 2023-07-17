a3cdc1d63d39f493ecd8cd5082307e165a41fc9f
Vivek Kapoor <er.vivek2512@gmail.com>
2023-07-17 20:45:02 +0530

    Changes in Normalize Grouping

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -588,7 +588,8 @@ route.get("/customers/customers/:id/:type", [authJWT.verifyToken, authJWT.isAdmi
         let list = [];
 
         if(typeof req.connection_db != "undefined" && req.connection_db != null ) {
-            if(typeof suggestions == 'undefined' && typeof fixed_identicals == 'undefined') { 
+            list = await helpers.findCompanyEntitiesByAccountID(organisationID, type, req.connection_db, suggestions, fixed_identicals); 
+            /* if(typeof suggestions == 'undefined' && typeof fixed_identicals == 'undefined') { 
                 list = await helpers.findCompanyEntitiesByAccountID(organisationID, type, req.connection_db, suggestions, fixed_identicals); 
             } else { 
                 console.log('Run File Account')
@@ -599,7 +600,7 @@ route.get("/customers/customers/:id/:type", [authJWT.verifyToken, authJWT.isAdmi
                     console.log('stderr', stderr)
                     return []
                 }) 
-            }
+            } */
         }
         res.status(200).json(list); 
     } catch (e){
@@ -657,13 +658,15 @@ route.get("/customers/customers/:id/:representativeID/:type", [authJWT.verifyTok
         let list = [];
 
         if(typeof req.connection_db != "undefined" && req.connection_db != null ) {
+            list = await helpers.findCompanyEntitiesByAccountIDByRepresentativeIDs(organisationID, representativeIDs, type, req.connection_db, suggestions, fixed_identicals);
+
             
-            if(typeof suggestions == 'undefined' && typeof fixed_identicals == 'undefined') { 
+            /* if(typeof suggestions == 'undefined' && typeof fixed_identicals == 'undefined') { 
                 list = await helpers.findCompanyEntitiesByAccountIDByRepresentativeIDs(organisationID, representativeIDs, type, req.connection_db, suggestions, fixed_identicals);
             } else { 
                 console.log('Run File')
                 exec(`/var/www/html/script/node_modules/.bin/env-cmd node /var/www/html/script/normalize_names.js ${req.orgId} ${req.params.representativeID}  ${type} ${suggestions} ${fixed_identicals}`);
-            } 
+            } */ 
         }
         res.status(200).json(list);
     } catch (e){
