2d247385b1386b1910fa50b7cfd7678ed54549f7
Vivek Kapoor <er.vivek2512@gmail.com>
2023-08-30 16:12:17 +0530

    Fixed Assets

--- routes/application/assets.js
+++ routes/application/assets.js
@@ -141,7 +141,7 @@ route.post("/assets/categories_products", [authJWT.verifyToken, clientDBConnecti
     }
 });
 
-route.post("/assets/cpc", [authJWT.verifyToken, clientDBConnection.connect], async(req, res, next) => {
+route.post("/assets/cpc", [authJWT.verifyToken, clientDBConnection.connect], async(req, res, next) => { 
     try{
         let { list, total, type, selectedCompanies, tabs, customers, assignments, range, scope, year, other_mode, data_type, sale, license, primary, check, lawfirm } = req.body, getList = [], group = [], sales = []
         const replacements = { organisation_id: req.orgId, year: 2000 }
@@ -157,10 +157,12 @@ route.post("/assets/cpc", [authJWT.verifyToken, clientDBConnection.connect], asy
             } else {
                 data_type = 1
             }
+        } else {
+            list = []
         }
 
-        replacements.type = helpers.findLayout(type); 
-        if(typeof type !== 'undefined' && type != 'due_dilligence' && list.length == 0) {
+        replacements.type = helpers.findLayout(type);  
+        if(typeof type !== 'undefined' && type != 'due_dilligence' && list.length == 0) { 
             if(type == 'top_law_firms') { 
                 let getFiilingAssets = []
                 if(typeof primary != 'undefined'){ 
