dd38a92e912d1db043018102f9d7f6bb86ecd53d
Vivek Kapoor <er.vivek2512@gmail.com>
2024-07-26 22:05:10 +0530

    Change DB config file

--- config/db.config.js
+++ config/db.config.js
@@ -1,127 +1,82 @@
 const Sequelize = require('sequelize');
-
 const moment = require('moment');
-
 const Op = Sequelize.Op;
 
 const application = new Sequelize(process.env.DATABASE_APPLICATION, process.env.USER, process.env.PASSWORD, {
     host: '167.172.195.92',
     dialect: 'mysql',
-    operatorsAliases: Op,
-   /*
-    pool: {
-      max: 1000,
-      min: 0,
-      acquire: 1000000,
-      idle: 10000
-    }*/
+    // Removed operatorsAliases
 });
 
 const applicationNew = new Sequelize(process.env.DATABASE_APPLICATION_NEW, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
-  dialect: 'mysql',
-  operatorsAliases: Op, 
+    host: '167.172.195.92',
+    dialect: 'mysql',
+    // Removed operatorsAliases
 });
 
-applicationNew.dialect.supports.schemas = true; 
+applicationNew.dialect.supports.schemas = true;
 
 const resources = new Sequelize(process.env.DATABASE_RAW, process.env.USER, process.env.PASSWORD, {
     host: '167.172.195.92',
     dialect: 'mysql',
-    operatorsAliases: Op,
-   /*
-    pool: {
-      max: 1000,
-      min: 0,
-      acquire: 1000000,
-      idle: 10000
-    }*/
+    // Removed operatorsAliases
 });
 
 const business = new Sequelize(process.env.DATABASE_BUSINESS, process.env.USER, process.env.PASSWORD, {
     host: '167.172.195.92',
     dialect: 'mysql',
-    operatorsAliases: Op,
-   /*
-    pool: {
-      max: 1000,
-      min: 0,
-      acquire: 1000000,
-      idle: 10000
-    }*/
+    // Removed operatorsAliases
 });
 
 const maintainence = new Sequelize(process.env.DATABASE_MAINTAINENCE, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
-  dialect: 'mysql',
-  operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+    host: '167.172.195.92',
+    dialect: 'mysql',
+    // Removed operatorsAliases
 });
 
 const biblioGrant = new Sequelize(process.env.DATABASE_GRANT_BIBLIO, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
-  dialect: 'mysql',
-  operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000, 
-    idle: 10000
-  }*/
+    host: '167.172.195.92',
+    dialect: 'mysql',
+    // Removed operatorsAliases
 });
 
 const biblioApplication = new Sequelize(process.env.DATABASE_APPLICATION_BIBLIO, process.env.USER, process.env.PASSWORD, {
-  host: '167.172.195.92',
-  dialect: 'mysql',
-  operatorsAliases: Op,
- /*
-  pool: {
-    max: 1000,
-    min: 0,
-    acquire: 1000000,
-    idle: 10000
-  }*/
+    host: '167.172.195.92',
+    dialect: 'mysql',
+    // Removed operatorsAliases
 });
 
 const config = {
-  'secret': process.env.SECRET || 'p@nt3nt8@60',
-  'pusher_appId': '938985',
-  'pusher_key': '3252bb191d77e92ddb3c',
-  'pusher_secret': '2a3dd823cd1abcd45c71',
-  'pusher_cluster': 'us3',
-  'pusher_encrypted': true,
-  'pusher_channel': 'patentrack-channel',
-  'pusher_event': 'patentrack-event',
+    'secret': process.env.SECRET || 'p@nt3nt8@60',
+    'pusher_appId': '938985',
+    'pusher_key': '3252bb191d77e92ddb3c',
+    'pusher_secret': '2a3dd823cd1abcd45c71',
+    'pusher_cluster': 'us3',
+    'pusher_encrypted': true,
+    'pusher_channel': 'patentrack-channel',
+    'pusher_event': 'patentrack-event',
 }
 
 const bucketConfig = {
-  bucketName: process.env.BUCKET_NAME,
-  dirName: process.env.BUCKET_PHOTO_DIR, /* optional */
-  region: process.env.BUCKET_REGION,
-  accessKeyId: process.env.BUCKET_ACCESS_KEY,
-  secretAccessKey: process.env.BUCKET_SECRET_KEY,
-  s3Url: process.env.BUCKET_URL, /* optional */
-  documentDir: process.env.BUCKET_DOCUMENT_DIR,
-  figuresDir: process.env.BUCKET_FIGURES_DIR,
+    bucketName: process.env.BUCKET_NAME,
+    dirName: process.env.BUCKET_PHOTO_DIR, /* optional */
+    region: process.env.BUCKET_REGION,
+    accessKeyId: process.env.BUCKET_ACCESS_KEY,
+    secretAccessKey: process.env.BUCKET_SECRET_KEY,
+    s3Url: process.env.BUCKET_URL, /* optional */
+    documentDir: process.env.BUCKET_DOCUMENT_DIR,
+    figuresDir: process.env.BUCKET_FIGURES_DIR,
 }
 
 const slackConfig = {
-  clientID: process.env.SLACK_CLIENT_ID,
-  clientSecret: process.env.SLACK_CLIENT_SECRET,
-  botToken: process.env.SLACK_BOT_TOKEN
+    clientID: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    botToken: process.env.SLACK_BOT_TOKEN
 }
 
-const DEFAULT_LIMIT = 100 
-
-const DEFAULT_YEAR = moment(new Date()).subtract(24, 'year').format('YYYY')
+const DEFAULT_LIMIT = 100;
+const DEFAULT_YEAR = moment(new Date()).subtract(24, 'year').format('YYYY');
 
 const db = { Sequelize, Op, application, applicationNew, resources, business, maintainence, biblioGrant, biblioApplication, config, bucketConfig, slackConfig, DEFAULT_LIMIT, DEFAULT_YEAR };
- 
-module.exports = db;  
+
+module.exports = db;
