39bd1c427c40a5e7f88dfc31f04bf6b7d1db73c9
Vivek Kapoor <er.vivek2512@gmail.com>
2024-08-13 21:52:56 +0530

    Fixed sentry packages

--- helpers/instrument.js
+++ helpers/instrument.js
@@ -4,10 +4,10 @@ const { nodeProfilingIntegration } = require("@sentry/profiling-node");
 // Ensure to call this before requiring any other modules!
 Sentry.init({
   dsn: "https://9dbb99721e484a939592c18830855a52@o487723.ingest.us.sentry.io/5547034",
-  integrations: [
+  /* integrations: [
     // Add our Profiling integration
     nodeProfilingIntegration(),
-  ],
+  ],  */
 
   // Add Tracing by setting tracesSampleRate
   // We recommend adjusting this value in production
