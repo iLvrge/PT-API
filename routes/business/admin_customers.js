225782c355889fa166196405a0d367815ec279ce
Vivek Kapoor <er.vivek2512@gmail.com>
2024-09-19 08:12:13 +0530

    Removed User from admin also removed from client account as well.

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -535,21 +535,31 @@ route.put("/users/:user_id", [authJWT.verifyToken, authJWT.isAdmin], async (req,
  * Delete admin user
  */
 
-route.delete("/users/:orgId/:user_id", [authJWT.verifyToken, authJWT.isAdmin], async (req, res, next) =>{
+route.delete("/users/:orgId/:user_id", [authJWT.verifyToken, authJWT.isAdmin, clientDBConnection.connect], async (req, res, next) =>{
     try{
         const user = await Users.findOne({
             where: {user_id: req.params.user_id, organisation_id: req.params.orgId}
         })
 
         if( user != null && user.user_id > 0){
-            
-            const deleteUser = await user.destroy();
+            const dbClientUser = await req.connection_db.define('Users', ClientUsers.mainStructure, ClientUsers.options);
 
-            if(deleteUser != null) {
-                res.status(200).send("User deleted successfully.");
+            const userDetail = await dbClientUser.findOne({
+                where: {user_id: req.params.user_id, role_id: 1},
+                attributes: ['user_id'],
+            });
+
+            if(userDetail) {
+                const deleteClientUser = await userDetail.destroy();
+                const deleteUser = await user.destroy();
+                if(deleteUser && deleteClientUser) {
+                    res.status(200).send("User deleted successfully.");
+                } else {
+                    res.status(500).send("Error while deleting user.");
+                }
             } else {
                 res.status(500).send("Error while deleting user.");
-            }
+            }  
         } else {
             res.status(500).send("Error while deleting user.");
         }
