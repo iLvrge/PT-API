2f71d63c57fca7c6cafc9cfc52561690a180edfc
Vivek Kapoor <er.vivek2512@gmail.com>
2025-04-23 08:39:13 +0530

    Load the environment file before run the script

--- routes/business/admin_customers.js
+++ routes/business/admin_customers.js
@@ -1873,7 +1873,7 @@ route.post("/customers", [authJWT.verifyToken, authJWT.isAdmin], async (req, res
                 //     }
                 // })
                
-                const cmd = `php -f ${process.env.SCRIPT_PATH}script_create_customer_db.php "${organisationID}"`;
+                const cmd = `screen -md bash -c "source ~/.bash_exports php -f ${process.env.SCRIPT_PATH}script_create_customer_db.php "${organisationID}"`;
                 console.log("Running:", cmd);
 
                 exec(cmd, (error, stdout, stderr) => {
