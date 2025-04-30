6c2c2a22356039c4711e604c0b58d48794d73943
Vivek Kapoor <er.vivek2512@gmail.com>
2025-04-30 06:40:32 +0530

    Redo the new connection work what previous developer did.

--- helpers/clientDBConnection.js
+++ helpers/clientDBConnection.js
@@ -3,18 +3,7 @@ const Sequelize = require('sequelize');
 const Op = Sequelize.Op;
 
 const helpers = require("./helper");
-
-let clientDB; // Store the client DB connection
-
-const process = require('process');
-
-process.on('exit', (code) => {
-    if(clientDB) {
-        console.log(`Closing client DB connection`);
-        clientDB.close();
-    }
-});
-
+ 
 const connect = async(req, res, next) => {
     let { check } = req.body
     
@@ -26,34 +15,31 @@ const connect = async(req, res, next) => {
              * Make DB Connection
              */
             try{
-                if (!clientDB) {
-                    clientDB = new Sequelize(organisation.org_db, organisation.org_usr, organisation.org_pass, {
-                        host: organisation.org_host,
-                        dialect: 'mysql',
-                        operatorsAliases: Op,
-                        pool: {
-                            max: 100,
-                            min: 1,
-                            acquire: 30000, // 30 seconds
-                            idle: 10000 // 10 seconds
+                const newConnection = new Sequelize(organisation.org_db, organisation.org_usr, organisation.org_pass, {
+                    host: organisation.org_host,
+                    dialect: 'mysql',
+                    operatorsAliases: Op,
+                    pool: {
+                        max: 100,
+                        min: 1,
+                        acquire: 30000, // 30 seconds
+                        idle: 10000 // 10 seconds
+                    },
+                    hooks: {
+                        // seems not working i'll keep it but anyway
+                        afterConnect: async (connection) => {
+                            connection.on('error', function(err){
+                                console.log(err.stack);
+                            });
+                            // console.log('Connection to database established successfully.');
                         },
-                        hooks: {
-                            // seems not working i'll keep it but anyway
-                            afterConnect: async (connection) => {
-                                connection.on('error', function(err){
-                                    console.log(err.stack);
-                                });
-                                // console.log('Connection to database established successfully.');
-                            },
-                        }
-                        
-                    });
-                }
-                req.connection_db = clientDB;
+                    }
+                    
+                });
+                req.connection_db = newConnection; 
             }catch( err ){
                 console.log(err);
-                req.connection_db = null;
-                clientDB = null; // Reset connection on error
+                req.connection_db = null; 
             }
         } else {
             /**
@@ -77,20 +63,17 @@ const connectOnFly = async(orgID) => {
             if( organisation != null && organisation.organisation_id > 0) {
                 /**
                  * Make DB Connection
-                 */
-                if (!clientDB) {
-                    clientDB = new Sequelize(organisation.org_db, organisation.org_usr, organisation.org_pass, {
-                        host: organisation.org_host,
-                        dialect: 'mysql',
-                        pool: {
-                            max: 100,
-                            min: 1,
-                            acquire: 30000, // 30 seconds
-                            idle: 10000 // 10 seconds
-                        }
-                    });
-                }
-                newConnection = clientDB;
+                 */ 
+                newConnection = new Sequelize(organisation.org_db, organisation.org_usr, organisation.org_pass, {
+                    host: organisation.org_host,
+                    dialect: 'mysql',
+                    pool: {
+                        max: 100,
+                        min: 1,
+                        acquire: 30000, // 30 seconds
+                        idle: 10000 // 10 seconds
+                    }
+                });
             }   
         }
     } catch (err) {
@@ -105,7 +88,6 @@ const clientDBConnection = {};
 clientDBConnection.connect = connect; 
 clientDBConnection.connectOnFly = connectOnFly; 
 clientDBConnection.Sequelize = Sequelize;
-clientDBConnection.Op = Op;
-clientDBConnection.clientDB = clientDB;
+clientDBConnection.Op = Op; 
   
 module.exports = clientDBConnection;
